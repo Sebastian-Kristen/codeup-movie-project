0d5d6df4131bc1bd32e2564eef6855fec37e3705
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-08-30 19:21:21 -0500

    Create all necessary files, include jQuery and Bootstrap in case we decide to use them, and add the to-do's as comments for easy reference

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// ***** ON PAGE LOAD *****
+// TODO: Display a "loading..." message
+// TODO: Make an AJAX request to get a listing of all the movies
+// TODO: When the initial AJAX request comes back, remove the "loading..." message and replace it with HTML generated from the json response your code receives
+
+// ***** ALLOW USERS TO ADD NEW MOVIES *****
+// TODO: Create a form for adding a new movie that has fields for the movie's title and rating
+// TODO: When the form is submitted, the page should not reload / refresh, instead, your javascript should make a POST request to /movies with the information the user put into the form
+
+// ***** ALLOW USERS TO EDIT EXISTING MOVIES *****
+// TODO: Give users the option to edit an existing movie
+// TODO: A form should be pre-populated with the selected movie's details
+// TODO: Like creating a movie, this should not involve any page reloads, instead your javascript code should make an ajax request when the form is submitted.
+
+// ***** DELETE MOVIES *****
+// TODO: Each movie should have a "delete" button
+// TODO: When this button is clicked, your javascript should send a DELETE request
